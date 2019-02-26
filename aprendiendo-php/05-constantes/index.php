3926d07d468f11209e38c73cbaab3896148350c8
Mike <lanselot777hailander@gmail.com>
2019-02-26 15:54:45 -0400

    constantes

---
+++ aprendiendo-php/05-constantes/index.php
@@ -0,0 +1,14 @@
+<?php
+  //Constantes
+  //Es un contenedor de informacion que no puede variar
+  define('nombre', 'Victor Robles');
+  define('web', 'victorroblesweb.es');
+
+  echo '<h1>'.nombre.'</h1>';
+  echo '<h1>'.web.'</h1>';
+
+  //Constantes predefinidas
+
+  echo __FILE__;
+
+ ?>
